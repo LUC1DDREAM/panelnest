802c66905829cc94691460bca2a8606f8f58f928
LUC1DDREAM <162044989+LUC1DDREAM@users.noreply.github.com>
2026-09-24 15:12:55 +0200

    Update HentaiFox version assertion

--- rust/multi.luc1d-hentaifox/src/tests.rs
+++ rust/multi.luc1d-hentaifox/src/tests.rs
@@ -608,7 +608,7 @@ fn manifest_preserves_identity_and_matches_discovery() {
 		serde_json::from_str(include_str!("../res/source.json")).unwrap();
 	assert_eq!(manifest["info"]["contentRating"], 2);
 	assert_eq!(manifest["info"]["languages"][0], "multi");
-	assert_eq!(manifest["info"]["version"], 19);
+	assert_eq!(manifest["info"]["version"], 20);
 	assert!(
 		manifest["info"]["name"]
 			.as_str()
