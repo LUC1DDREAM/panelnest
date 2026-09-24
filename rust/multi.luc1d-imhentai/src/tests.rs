a1ccff9bb5fb389904a337f7541e719189083151
LUC1DDREAM <162044989+LUC1DDREAM@users.noreply.github.com>
2026-09-24 08:47:40 +0200

    Fix IMHentai version assertions

--- rust/multi.luc1d-imhentai/src/tests.rs
+++ rust/multi.luc1d-imhentai/src/tests.rs
@@ -220,7 +220,7 @@ fn manifest_preserves_identity_and_matches_discovery() {
 		serde_json::from_str(include_str!("../res/source.json")).unwrap();
 	assert_eq!(manifest["info"]["contentRating"], 2);
 	assert_eq!(manifest["info"]["languages"][0], "multi");
-	assert_eq!(manifest["info"]["version"], 9);
+	assert_eq!(manifest["info"]["version"], 10);
 	assert!(
 		manifest["info"]["name"]
 			.as_str()
@@ -230,7 +230,7 @@ fn manifest_preserves_identity_and_matches_discovery() {
 	for listing in manifest["listings"].as_array().unwrap() {
 		assert!(listing_url(listing["id"].as_str().unwrap(), 1).is_ok());
 	}
-	assert_eq!(manifest["info"]["version"], 9);
+	assert_eq!(manifest["info"]["version"], 10);
 }
 
 use super::*;
