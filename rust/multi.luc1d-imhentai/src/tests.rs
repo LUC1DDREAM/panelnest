7e0efb7df02e5c9a456634d7a95e6b40937a2a92
LUC1DDREAM <162044989+LUC1DDREAM@users.noreply.github.com>
2026-09-24 06:47:38 +0200

    test: expect IMHentai v8 manifest

--- rust/multi.luc1d-imhentai/src/tests.rs
+++ rust/multi.luc1d-imhentai/src/tests.rs
@@ -170,7 +170,7 @@ fn manifest_preserves_identity_and_matches_discovery() {
 		serde_json::from_str(include_str!("../res/source.json")).unwrap();
 	assert_eq!(manifest["info"]["contentRating"], 2);
 	assert_eq!(manifest["info"]["languages"][0], "multi");
-	assert_eq!(manifest["info"]["version"], 7);
+	assert_eq!(manifest["info"]["version"], 8);
 	assert!(manifest["info"]["name"].as_str().unwrap().ends_with(" [PN]"));
 	for listing in manifest["listings"].as_array().unwrap() {
 		assert!(listing_url(listing["id"].as_str().unwrap(), 1).is_ok());
